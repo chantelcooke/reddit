607135e20c3a105c18fd3a9089e744a3d5cc3d26
Chantel Cooke <chantelmcooke@gmail.com>
2021-11-24 20:56:45 -0700

    sub controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# route is going to be the http request and do crud actions in the browser
+# lead somewhere in the controller
+# combination of the url and http request
+
+# root 'controller#action' - home page to the url pattern
+#root 'static#home'
+# resources :controller name - generate all the routes and actions for a controller
+#resources :contacts
+# - with relationships with models you are going to embed two levels deep
+#resources :contacts do
+  #resources :notes
+#end  
+#doing the routes by hand
+
+#httpver 'pattern', goes to: 'controller#actions'
+# get       Read
+# post        create
+# put / patch   update
+# delete          destroy
+
+  root 'subs#index'
+
+  resources :subs
+
 end
